dfc6710c31b22f3a08594868c6f0e9861a5cec4a
Resilient Admin User <resadmin@resilient.localdomain>
2020-08-28 00:07:16 +0800

    able to find names

---
+++ fn_notify_user/fn_notify_user/util/config.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Generate a default configuration-file section for fn_notify_user"""
+
+
+def config_section_data():
+    """
+    Produce add the default configuration section to app.config,
+    for fn_notify_user when called by `resilient-circuits config [-c|-u]`
+    """
+    config_data = None
+
+#    config_data = u"""[fn_notify_user]
+#setting=xxx
+#"""
+    return config_data
